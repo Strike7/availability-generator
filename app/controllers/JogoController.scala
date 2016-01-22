18495b0bb2ca1532a7d4a39ca61a0c2f692b4d8b
Onezino Moreira <zinogabriel@gmail.com>
2016-01-22 17:03:37 -0200

    implementa coluna data ao jogo referente a data da reserva

--- app/controllers/JogoController.scala
+++ app/controllers/JogoController.scala
@@ -1,5 +1,9 @@
 package controllers
 
+import java.util.Date
+
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 import play.api.db.slick.DBAction
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
@@ -11,21 +15,29 @@ import play.api.Play.current
 
 
 object JogoController extends Controller {
+  val dateFormat = DateTimeFormat.forPattern("yyyy-MM-dd")
+  val jodaDateReads = Reads[DateTime](js =>
+    js.validate[String].map[DateTime](dtString =>
+      dateFormat.parseDateTime(dtString)
+    )
+  )
 
-    implicit val JogoWrites = new Writes[Jogo]{
-      override def writes(jogo: Jogo): JsValue = Json.obj(
-        "id" -> JsNumber(jogo.id.get),
-        "titulo" -> JsString(jogo.titulo),
-        "capa" -> JsString(jogo.capa),
-        "disponibilidade" -> JsBoolean(jogo.disponibilidade)
-      )
-    }
+  implicit val JogoWrites = new Writes[Jogo]{
+    override def writes(jogo: Jogo): JsValue = Json.obj(
+      "id" -> JsNumber(jogo.id.get),
+      "titulo" -> JsString(jogo.titulo),
+      "capa" -> JsString(jogo.capa),
+      "data" -> JsString(dateFormat.print( jogo.data )),
+      "disponibilidade" -> JsBoolean(jogo.disponibilidade)
+    )
+  }
   implicit val jogoReads : Reads[Jogo] =
     (
         (JsPath \ "id" ).readNullable[Long] and
         (JsPath \ "titulo" ).read[String] and
         (JsPath \ "capa" ).read[String] and
-          (JsPath \ "disponibilidade").read[Boolean]
+        (JsPath \ "data" ).read[DateTime](jodaDateReads ) and
+        (JsPath \ "disponibilidade").read[Boolean]
       )(Jogo.apply _)
 
     def todos = DBAction { implicit rs =>
@@ -46,6 +58,7 @@ object JogoController extends Controller {
 
         val jogoBanco = Jogos.get(id)
         jogoBanco.disponibilidade = jogo.disponibilidade
+        jogoBanco.data = jogo.data
         Jogos.update(id, jogoBanco)
         Ok(Json.toJson(jogoBanco))
       }
--- app/models/Jogo.scala
+++ app/models/Jogo.scala
@@ -1,21 +1,42 @@
 package models
 
+import java.sql.Timestamp
+import java.text.{SimpleDateFormat, DateFormat}
+import java.util.Date
+
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 import play.api.Play.current
 import play.api.db.slick.Config.driver.simple._
 import scala.slick.lifted.Tag
 
-case class Jogo(id: Option[Long], titulo: String, capa: String, var disponibilidade: Boolean) {
+object date {
+  val df = DateTimeFormat.forPattern("DDMM")
+
+  def capaData(d: DateTime) = {
+    df.print(d);
+  }
+}
+case class Jogo(id: Option[Long], titulo: String, capa: String,var data: DateTime,  var disponibilidade: Boolean) {
 
   def capaNome = if (disponibilidade) capa else "ua-" + capa
+  def capaData = date.capaData(data)
 }
 
 class Jogos(tag: Tag) extends Table[Jogo](tag, "JOGOS") {
+  implicit def DateTime2TimeStamp = MappedColumnType.base[DateTime, Timestamp]({
+    dt => new Timestamp(dt.getMillis )
+  }, {
+    tt => new DateTime(tt)
+  })
+
   def id = column[Option[Long]]("id", O.PrimaryKey, O.AutoInc)
   def titulo = column[String]("titulo", O.NotNull)
   def capa = column[String]("capa", O.NotNull)
+  def data = column[DateTime]("data")
   def disponibilidade = column[Boolean]("disponibilidade", O.NotNull)
 
-  override def * = (id, titulo, capa, disponibilidade) <> (Jogo.tupled, Jogo.unapply _)
+  override def * = (id, titulo, capa, data, disponibilidade) <> (Jogo.tupled, Jogo.unapply _)
 }
 
 
